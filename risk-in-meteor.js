fa0b6f75eaeff929f4c232e1b8ae08291b13cb90
Ian Zabel <ian@thoughtbot.com>
2014-12-11 15:57:02 -0500

    cleanups

--- risk-in-meteor.js
+++ risk-in-meteor.js
@@ -132,27 +132,13 @@ if (Meteor.isClient) {
     d3.select(self.frameElement).style("height", height + "px");
 
   }
-
-  // Template.hello.helpers({
-  //   counter: function () {
-  //     return Session.get("counter");
-  //   }
-  // });
-
-  // Template.hello.events({
-  //   'click button': function () {
-  //     // increment the counter when button is clicked
-  //     Session.set("counter", Session.get("counter") + 1);
-  //   }
-  // });
 }
 
 if (Meteor.isServer) {
 
   Meteor.startup(function () {
 
-    Player.update(
-      {
+    Player.update({
         name: "Player 1",
       },
       {
