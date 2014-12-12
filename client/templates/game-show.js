272811b9661bc8cfa282f8577f6b7755f7ad2e6e
Ian Zabel <ian@thoughtbot.com>
2014-12-12 11:18:58 -0500

    Show attack targets

--- client/templates/game-show.js
+++ client/templates/game-show.js
@@ -7,5 +7,14 @@ Template.game.helpers({
     return occupations.map(function (occupation) {
       return Territory.findOne(occupation.territory);
     });
+  },
+  targets: function() {
+    var targets = Occupation.find({
+      player: { $not: this.currentPlayer._id },
+      armies: {$gt: 1}
+    });
+    return targets.map(function (target) {
+      return Territory.findOne(target.territory);
+    });
   }
 });
