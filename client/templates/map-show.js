ea1e0e90e424f0a1fe5b8fda0973aed47fb4e857
Ward Penney <ward@thoughtbot.com>
2014-12-12 11:43:33 -0500

    simple territory clicking

--- client/templates/map-show.js
+++ client/templates/map-show.js
@@ -25,8 +25,51 @@ Template.map.rendered = function () {
       .enter().insert("path", ".graticule")
         .attr("class", "territory")
         .attr("d", path)
-        .attr("data-territory", function(d) { return d.id; });
+        .attr("data-territory", function(d) { return d.id; })
+        .on("click", function (territory, index) {
+          d3.event.stopPropagation();
+          window.TerritoryUI.territoryClicked(d3.event.target.getAttribute("data-territory"));
+        })
   });
 
   d3.select(self.frameElement).style("height", height + "px");
+
+  $("#map svg").click(function() {
+    window.TerritoryUI.clearTerritories();
+  });
+}
+
+window.TerritoryUI = {
+  firstTerritory: "",
+  secondTerritory: "",
+  assignFirstTerritory: function(territory) {
+    $("[data-js~='first-territory']").text(territory)
+    $("#map svg path[data-territory='" + territory + "']").attr("class", "territory territory--first");
+    $("#map svg").attr("class", "first-territory-on");
+  },
+  assignSecondTerritory: function(territory) {
+    $("[data-js~='second-territory']").text(territory)
+    $("#map svg path[data-territory='" + territory + "']").attr("class", "territory territory--second");
+    $("#map svg").attr("class", "first-territory-on second-territory-on");
+  },
+  clearTerritories: function() {
+    this.firstTerritory = "";
+    $("[data-js~='first-territory']").text("")
+
+    this.secondTerritory = "";
+    $("[data-js~='second-territory']").text("")
+
+    $("#map svg path").attr("class", "territory");
+    $("#map svg").attr("class", "");
+  },
+  territoryClicked: function (territory) {
+    if (this.firstTerritory === "") {
+      this.firstTerritory = territory;
+      this.assignFirstTerritory(territory);
+    } else if (this.secondTerritory === "" &&
+               this.firstTerritory != territory) {
+      this.assignSecondTerritory(territory);
+      this.secondTerritory = territory;
+    }
+  }
 }
